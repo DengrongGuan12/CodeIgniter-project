4c672a5ebc644d2d26b99e7d586d67d3eb150b72
DengrongGuan <15950570277@163.com>
2014-09-20 21:27:33 +0800

    p21

--- application/controllers/blog.php
+++ application/controllers/blog.php
@@ -39,6 +39,50 @@ class Blog extends CI_Controller
 
 		redirect('blog/comments/'.$_POST['entry_id']);
 	}
+    /*
+     * 数据库查询语句的用法
+     */
+    function query_test(){
+        $this->db->select('url','name','clientid','people.surnam
+        e AS client');
+        $this->db->where('clientid', '3');
+        $this->db->limit(5);
+        $this->db->from('sites');
+        $this->db->join('people', 'sites.clientid = people.id');
+        $this->db->orderby("name", "desc");
+        $query = $this->db->get();
+        foreach ($query->result() as $row)
+        {
+            print $row->url;
+            print $row->name;
+            print $row->client;
+        }
+
+    }
+    function insert_test(){
+        $data = array(
+            'url' => 'www.mynewclient.com',
+            'name' => 'BigCo Inc',
+            'clientid' => '33',
+            'type' => 'dynamic'
+        );
+        $this->db->insert('sites', $data);
+        $new_id_number = $this->db->insert_id();
+    }
+    function delete_test(){
+        $this->db->where('id', '2');
+        $this->db->delete('sites');
+    }
+    function update_test(){
+        $data = array(
+            'url' => 'www.mynewclient.com',
+            'name' => 'BigCo Inc',
+            'clientid' => '33',
+            'type' => 'dynamic'
+        );
+        $this->db->where('id', '1');
+        $this->db->update('sites', $data);
+    }
 }
 
 ?>
